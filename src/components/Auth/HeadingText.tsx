6a8a393c7c527105b8d3b74232f86ab9981ceb0f
Bipul Kumar <bknor951@gmail.com>
2024-05-20 19:54:33 +0530

    first-commit

---
+++ src/components/Auth/HeadingText.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const HeadingText = () => {
+  return (
+    <View>
+      <Text>HeadingText</Text>
+    </View>
+  );
+};
+
+export default HeadingText;
+
+const styles = StyleSheet.create({});
